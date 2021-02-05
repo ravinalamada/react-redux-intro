6163cdb011c375d3c1ecc1c80a83dfd09efe4527
Prisca Roseline <prisca.ros@onja.org>
2021-02-05 09:57:09 +0300

    Exercise 05/02/21 about connect.

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import React from 'react';
-import store from './redux';
+import { useSelector, useDispatch } from 'react-redux';
 import {increment, decrement} from './redux/count';
 
-function App(props) {
-  store.dispatch(increment(3))
-  store.dispatch(decrement(2))
-	return <div>Cool</div>;
+function App() {
+  const count =  useSelector(state => state.count);
+  const dispatch = useDispatch()
+	return (
+    <div>
+      <h1>{count}</h1>
+     <button onClick={() => dispatch(increment())}>+</button>
+     <button onClick={() => dispatch(decrement())}>-</button>
+    </div>
+  )
 }
+
+// const mapStateToProps = (state) => {
+//   return {
+//     count: state.count
+//   }
+// }
+
+// const mapToDispatch = {
+//   increment,
+//   decrement,
+// }
+
+// export default connect(state =>({count: state.count}) , {increment, decrement})(App)
 
 export default App;
